98fd9184db8b6671a3f36ebd4b714fa9b7be7ad8
MacKenzie <mackenzieajudd@gmail.com>
2016-01-30 19:33:13 -0800

    Fixed ticking bug

--- src/js/components/ReactGameOfLife.jsx
+++ src/js/components/ReactGameOfLife.jsx
@@ -6,22 +6,21 @@ var ReactGameOfLife = React.createClass({
   getInitialState: function() {
     return {lifeBoard: new LifeBoard(40, 40, [{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 3}])};
   },
+  componentDidMount: function(){
+    var that = this;
+    setInterval(function(){
+        var lifeBoard = that.state.lifeBoard;
+        lifeBoard.tick();
+        that.setState({lifeBoard: lifeBoard});
+    }, 1000);
+  },
   makeAlive: function(coordinates){
     var lifeBoard = this.state.lifeBoard;
     lifeBoard.makeAlive(coordinates);
     this.setState({lifeBoard: lifeBoard});
   },
   render: function() {
-    
-    var that = this;
-    setTimeout(function(){
-        var lifeBoard = that.state.lifeBoard;
-        lifeBoard.tick();
-        that.setState({lifeBoard: lifeBoard});
-    }, 5000);
-    
     var board = this.state.lifeBoard.getBoard();
-    
     var content = [];
     
     var ySize = board[0].length;
